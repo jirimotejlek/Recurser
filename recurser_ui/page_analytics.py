4c85a53d7cda852778aff858babf13ca869ebfd8
Jiri <jiri.motejlek@redshiftedgames.com>
2025-07-29 15:31:58 +0100

    Added test button for scraper

--- recurser_ui/page_analytics.py
+++ recurser_ui/page_analytics.py
@@ -5,6 +5,8 @@ import requests
 
 SEARCH_ENGINE = os.getenv('SEARCH_ENGINE', 'search-engine')
 SEARCH_ENGINE_PORT = os.getenv('SEARCH_ENGINE_PORT', '5150')
+SCRAPER = os.getenv('SCRAPER', 'scraper')
+SCRAPER_PORT = os.getenv('SCRAPER_PORT', '5200')
 
 def streamlit_page():
     st.title("📊 Analytics Dashboard")
@@ -12,6 +14,16 @@ def streamlit_page():
     if st.button("Test web search"):
         with st.spinner("Searching..."):
             response = requests.post(f"http://{SEARCH_ENGINE}:{SEARCH_ENGINE_PORT}/query", timeout=60, json={"search_query": "What is the capital of France?"})
+            st.write(response)
+            data = response.json()
+            if 'response' in data:
+                st.write(data['response'])
+            else:
+                st.error(f"❌ Error: {'Unknown error'}")
+
+    if st.button("Test web scraper"):
+        with st.spinner("Searching..."):
+            response = requests.post(f"http://{SCRAPER}:{SCRAPER_PORT}/scrape", timeout=60, json={"url": "https://www.google.com"})
             st.write(response)
             data = response.json()
             if 'response' in data:
